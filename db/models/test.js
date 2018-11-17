810168dfbb0d76cc4705b2540666e829eda7a185
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-11-17 10:37:50 -0800

    Models Complete

--- db/models/test.js
+++ db/models/test.js
@@ -1,8 +1,19 @@
-'use strict';
-const Sequelize = require('sequelize');
-const db = require('../db');
-const Student = require('./student');
+"use strict";
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Student = require("./student");
 
-const Test = db.define();
+const Test = db.define("test", {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+Test.belongsTo(Student);
 
 module.exports = Test;
